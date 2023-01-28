8b364ef4a29ec486dc12bf88269500fabde990d8
Roberto <rgalanllorca@me.com>
2023-01-28 20:09:34 +0100

    Arreglando cosas

--- includes/database.php
+++ includes/database.php
@@ -1,17 +1,11 @@
 <?php
 
-$db = mysqli_connect(
-    $_ENV['DB_HOST'], 
-    $_ENV['DB_USER'], 
-    $_ENV['DB_PASS'] ?? '',  
-    $_ENV['DB_BD'], 
-);
-
+$db = mysqli_connect($_ENV['DB_HOST'],$_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']); 
 $db->set_charset("utf8");
 
-if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "error de depuración: " . mysqli_connect_error();
-    echo "error de depuración: " . mysqli_connect_error();
+if (!$db) { 
+    echo "Error: No se pudo conectar a MySQL."; 
+    echo "errno de depuración: " . mysqli_connect_errno(); 
+    echo "error de depuración: " . mysqli_connect_error(); 
     exit;
-}
+}  
